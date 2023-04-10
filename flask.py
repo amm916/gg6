edf8ebeaaa3309b834cdb58be8b23e71406104d0
b1nalidev <b1nali.discord@gmail.com>
2023-04-10 19:30:41 +0300

    Note to Dr. Sami:- This is a blog post data including the ID, title, content, date and the author.

--- flask.py
+++ flask.py
@@ -1,1 +1,30 @@
-a
+from datetime import datetime
+from flask import Flask, render_template, request, redirect, url_for
+import smtplib
+
+app = Flask(__name__)
+
+# Define the post data
+posts = [
+    {
+        "id": 1,
+        "title": "My first blog post",
+        "content": "This is the content of my first blog post.",
+        "date_posted": datetime(2023, 4, 1),
+        "author": "Ammar"
+    },
+    {
+        "id": 2,
+        "title": "My second blog post",
+        "content": "This is the content of my second blog post.",
+        "date_posted": datetime(2023, 4, 2),
+        "author": "Shaker"
+    },
+    {
+        "id": 3,
+        "title": "My third blog post",
+        "content": "This is the content of my third blog post.",
+        "date_posted": datetime(2023, 4, 3),
+        "author": "Amjad"
+    }
+]
