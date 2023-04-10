10270ea3962b066635b27a111ebbc305df0a436f
FaisalShaker <faisalshaker55@gmail.com>
2023-04-10 19:48:29 +0300

    This is a Python Flask code that defines routes for a web application, which render HTML templates and handle GET and POST requests, including sending an email from a contact form.

--- flask.py
+++ flask.py
@@ -28,3 +28,46 @@ posts = [
         "author": "Amjad"
     }
 ]
+# Define the route for the home page
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+# Define the route for the posts page
+@app.route("/posts")
+def posts():
+    return render_template("posts.html", posts=posts)
+
+# Define the route for a single post page
+@app.route("/posts/<int:id>")
+def post(id):
+    post = next((p for p in posts if p["id"] == id), None)
+    if post:
+        return render_template("post.html", post=post)
+    else:
+        return render_template("404.html"), 404
+
+# Define the route for the contact us page
+@app.route("/contact", methods=["GET", "POST"])
+def contact():
+    if request.method == "POST":
+        name = request.form.get("name")
+        email = request.form.get("email")
+        message = request.form.get("message")
+
+        # Send the email
+        with smtplib.SMTP("smtp.gmail.com", 587) as smtp:
+            smtp.starttls()
+            smtp.login("youremail@gmail.com", "yourpassword")
+            subject = f"New message from {name}"
+            body = f"Name: {name}\nEmail: {email}\n\n{message}"
+            smtp.sendmail("youremail@gmail.com", "recipientemail@example.com", f"Subject: {subject}\n\n{body}")
+
+        return redirect(url_for("thankyou"))
+
+    return render_template("contactus.html")
+
+# Define the route for the thank you page
+@app.route("/thankyou")
+def thankyou():
+    return render_template("thankyou.html")
