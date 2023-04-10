d056eeb0412d2fd657c61dfd43af56b86bd81697
amm916 <alahmadi916@yahoo.com>
2023-04-10 05:25:29 +0300

    تعديل اخطاء كثيره

---
+++ recqrst-data.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+    $.ajax({
+        url: "mobile-data.json",
+        dataType: "json",
+        success: function(data) {
+            var html = "";
+            $.each(data, function(index, item) {
+                html += `
+            <div class="card">
+              <img class="card-img-top" src="${item.image}" alt="${item.title}">
+              <div class="card-body">
+                <h5 class="card-title">${item.title}</h5>
+                <p class="card-text">${item.description}</p>
+                <a href="#" class="btn btn-primary">${item.price}</a>
+              </div>
+            </div>
+          `;
+            });
+            $("#mobile-container").html(html);
+        },
+        error: function(jqXHR, textStatus, errorThrown) {
+            console.log("Error: " + textStatus + " - " + errorThrown);
+        }
+    });
+});
