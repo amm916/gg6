b20b21c43b95c97dcd8c519cd944e5bf7ced7ea1
amm916 <alahmadi916@yahoo.com>
2023-04-13 21:18:31 +0300

    الانتهاء من تقسيم الملفات

--- recqrst-data.js
+++ recqrst-data.js
@@ -6,15 +6,15 @@ $(document).ready(function() {
             var html = "";
             $.each(data, function(index, item) {
                 html += `
-            <div class="card">
-              <img class="card-img-top" src="${item.image}" alt="${item.title}">
-              <div class="card-body">
-                <h5 class="card-title">${item.title}</h5>
-                <p class="card-text">${item.description}</p>
-                <a href="#" class="btn btn-primary">${item.price}</a>
-              </div>
+          <div class="card">
+            <img class="card-img-top" src="${item.image}" alt="${item.title}">
+            <div class="card-body">
+              <h5 class="card-title">${item.title}</h5>
+              <p class="card-text">${item.description}</p>
+              <a href="#" class="btn btn-primary">${item.price}</a>
             </div>
-          `;
+          </div>
+        `;
             });
             $("#mobile-container").html(html);
         },
