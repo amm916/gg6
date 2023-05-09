05d95206f75b4e8732054627d548a77e36e4f31a
ALGOTHMI <ab.algothmi@hotmail.com>
2023-05-09 09:13:58 +0300

    fixe the email

--- flask.py
+++ flask.py
@@ -1,33 +1,19 @@
 from datetime import datetime
 from flask import Flask, render_template, request, redirect, url_for
-import smtplib
+from flask_mail import Mail, Message
 
 app = Flask(__name__)
 
-# Define the post data
-posts = [
-    {
-        "id": 1,
-        "title": "My first blog post",
-        "content": "This is the content of my first blog post.",
-        "date_posted": datetime(2023, 4, 1),
-        "author": "Ammar"
-    },
-    {
-        "id": 2,
-        "title": "My second blog post",
-        "content": "This is the content of my second blog post.",
-        "date_posted": datetime(2023, 4, 2),
-        "author": "Shaker"
-    },
-    {
-        "id": 3,
-        "title": "My third blog post",
-        "content": "This is the content of my third blog post.",
-        "date_posted": datetime(2023, 4, 3),
-        "author": "Amjad"
-    }
-]
+# Define the configuration for Flask-Mail
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = 'youremail@gmail.com'
+app.config['MAIL_PASSWORD'] = 'yourpassword'
+
+# Define the mail object for sending emails
+mail = Mail(app)
+
 # Define the route for the home page
 @app.route("/")
 def index():
@@ -36,14 +22,14 @@ def index():
 # Define the route for the posts page
 @app.route("/posts")
 def posts():
-    return render_template("posts.html", posts=posts)
+    return render_template("posts-all.html", posts=posts)
 
 # Define the route for a single post page
 @app.route("/posts/<int:id>")
 def post(id):
     post = next((p for p in posts if p["id"] == id), None)
     if post:
-        return render_template("post.html", post=post)
+        return render_template("post-single.html", post=post)
     else:
         return render_template("404.html"), 404
 
@@ -55,17 +41,17 @@ def contact():
         email = request.form.get("email")
         message = request.form.get("message")
 
-        # Send the email
-        with smtplib.SMTP("smtp.gmail.com", 587) as smtp:
-            smtp.starttls()
-            smtp.login("youremail@gmail.com", "yourpassword")
-            subject = f"New message from {name}"
-            body = f"Name: {name}\nEmail: {email}\n\n{message}"
-            smtp.sendmail("youremail@gmail.com", "recipientemail@example.com", f"Subject: {subject}\n\n{body}")
-
-        return redirect(url_for("thankyou"))
-
-    return render_template("contactus.html")
+    @app.route('/send_email', methods=['POST'])
+    def send_reset_email():
+        user_email = request.form['user_email']
+    msg = Message('title of  email',
+                  sender='noreply@demo.com',
+                  recipients=['Ubtalubt@gmail.com'])
+    msg.body = f'''
+        Hello {'Ubtalubt@gmail.com'},
+    '''
+    mail.send(msg)
+    return 'success!'
 
 # Define the route for the thank you page
 @app.route("/thankyou")
@@ -81,3 +67,6 @@ def not_found_error(error):
 @app.errorhandler(500)
 def internal_error(error):
     return render_template("500.html"), 500
+
+if __name__ == '__main__':
+    app.run(debug=True)
