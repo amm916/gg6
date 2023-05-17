502c1b1b5124fd44d60d9f1f82a2bef6140c49b9
amm916 <alahmadi916@yaoo.com>
2023-05-17 15:47:17 +0300

    تصليح المشروع بالكامل

--- app.py
+++ app.py
@@ -1,63 +1,76 @@
-from datetime import datetime
-from app import Flask, render_template, request, redirect, url_for
+from flask import Flask,render_template
+from postsdata import posts
 
 app = Flask(__name__)
 
-# Define the configuration for Flask-Mail
-app.config['MAIL_SERVER'] = 'smtp.gmail.com'
-app.config['MAIL_PORT'] = 587
-app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = 'youremail@gmail.com'
-app.config['MAIL_PASSWORD'] = 'yourpassword'
-
-# Define the mail object for sending emails
-
-# Define the route for the home page
+# Page Routes
 @app.route("/")
-def index():
-    return render_template("home.html")
+def homePagex():
+   return render_template('home.html')
 
-# Define the route for the posts page
-@app.route("/posts")
-def posts():
-    return render_template("posts-all.html", posts=posts)
+@app.route("/about")
+def about():
+    return render_template('about.html')
+        
 
-# Define the route for a single post page
-@app.route("/posts/<int:id>")
-def post(id):
-    post = next((p for p in posts if p["id"] == id), None)
-    if post:
-        return render_template("post-single.html", post=post)
-    else:
-        return render_template("404.html"), 404
-
-# Define the route for the contact us page
-@app.route("/contact", methods=["GET", "POST"])
+@app.route("/contact")
 def contact():
-    if request.method == "POST":
-        name = request.form.get("name")
-        email = request.form.get("email")
-        message = request.form.get("message")
-
-    @app.route('/send_email', methods=['POST'])
-    def send_reset_email():
-        user_email = request.form['user_email']
+    return render_template('contact.html')
 
 
-# Define the route for the thank you page
-@app.route("/thankyou")
-def thankyou():
-    return render_template("thankyou.html")
 
-# Define the route for the 404 page
-@app.errorhandler(404)
-def not_found_error(error):
-    return render_template("404.html"), 404
+@app.route("/reqres-data")
+def reqresData():
+    return render_template('recqrst-data.html')
 
-# Define the route for the server error page
-@app.errorhandler(500)
-def internal_error(error):
-    return render_template("500.html"), 500
+@app.route("/posts")
+def home():
+    return render_template('post-all.html',
+                           title='all posts',
+                           posts=posts)
 
+
+@app.route('/posts/<int:post_id>')
+def show_post(post_id):
+    if post_id < len(posts):
+       p = posts[post_id]
+       return render_template('postsingle.html',
+       title= f"Post#{post_id}", p = p )
+    else:
+        return render_template('404.html'), 404
+
+    
+#to get favicon working on layout instead of html page
+import os
+from flask import send_from_directory
+from json import dumps
+
+
+
+#Implementing flask to the second form of the contact us page
+@app.route('/contact-us', methods=['POST'])
+def contact_us():
+    name = request.form['Contact-Name']
+    email = request.form['Contact-Email']
+    message = request.form['Contact-Message']
+
+    # Send email using Flask-Mail
+    msg = Message('Contact Us Form Submission',
+                  sender='noreply@demo.com',
+                  recipients=['ubttester1@gmail.com'])
+    msg.body = f'''
+        Name: {name}
+        Email: {email}
+        Message: {message}
+    '''
+    mail.send(msg)
+
+    # Return a success message to the user
+    return 'Thank you for your message!'\
+        '<a href="/contact"> Click here to submit another message.'\
+        '</a> <br> Or click <a href="/index">Home</a> to return to the main page'
+
+#debug tool
 if __name__ == '__main__':
-    app.run(debug=True)
+	app.run( debug=True )
+
---
+++ postsdata.py
@@ -0,0 +1,29 @@
+# postsdata.py
+
+# Sample data
+posts = [
+    {'title': 'Post 1', 'content': 'marwan'},
+    {'title': 'Post 2', 'content': 'This is post 2 content'},
+    {'title': 'Post 3', 'content': 'This is post 3 content'},
+]
+# postsdata.py
+
+# List of posts
+posts = [
+    {
+        'id': 1,
+        'title': 'Post 1',
+        'content': 'This is the content of post 1.'
+    },
+    {
+        'id': 2,
+        'title': 'Post 2',
+        'content': 'This is the content of post 2.'
+    },
+    {
+        'id': 3,
+        'title': 'Post 3',
+        'content': 'This is the content of post 3.'
+    },
+    # Add more posts as needed
+]
